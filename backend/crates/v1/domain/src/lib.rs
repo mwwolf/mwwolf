d9ba858bee99bb8e3b54d649b04ba4ecba218e5e
ryutah <6662577+ryutah@users.noreply.github.com>
2021-07-22 19:25:31 +0900

    feat(domain): define player domain model

--- backend/crates/v1/domain/src/lib.rs
+++ backend/crates/v1/domain/src/lib.rs
@@ -1,17 +1,22 @@
-// #[macro_use]
+#[macro_use]
 extern crate derive_new;
 
 #[macro_use]
 extern crate derive_getters;
+
+#[macro_use]
+extern crate strum_macros;
 
 // #[macro_use]
 extern crate async_trait;
 
 mod error;
 mod id;
+mod player;
 mod result;
 
 pub use error::*;
 pub use id::Id;
 // use libmww::*;
+pub use player::*;
 pub use result::{DomainResult, RepositoryResult};
---
+++ backend/crates/v1/domain/src/player.rs
@@ -0,0 +1,59 @@
+use libmww::id::Id;
+
+use crate::{DomainError, DomainErrorKind, DomainResult};
+
+#[derive(EnumString, IntoStaticStr)]
+pub enum PlayerRole {
+    #[strum(serialize = "host")]
+    Host,
+    #[strum(serialize = "guest")]
+    Guest,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PlayerName(String);
+
+impl From<PlayerName> for String {
+    fn from(n: PlayerName) -> Self {
+        n.0
+    }
+}
+
+impl PlayerName {
+    pub fn try_new(name: impl Into<String>) -> DomainResult<PlayerName> {
+        let name = name.into();
+        if name.is_empty() {
+            Err(DomainError::new(
+                DomainErrorKind::InvalidInput,
+                "name should not be blank",
+            ))
+        } else {
+            Ok(Self(name))
+        }
+    }
+}
+
+#[derive(new, Getters)]
+pub struct Player {
+    id: Id<Player>,
+    role: PlayerRole,
+    name: PlayerName,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use test_case::test_case;
+
+    #[test_case("name" => Ok(PlayerName("name".into())))]
+    #[test_case("" => Err(DomainError::new(DomainErrorKind::InvalidInput, "name should not be blank")))]
+    fn player_name_try_new_test(name: &str) -> DomainResult<PlayerName> {
+        PlayerName::try_new(name)
+    }
+
+    #[test_case(PlayerName("name".into()) => "name".to_string())]
+    fn player_name_into_string(player_name: PlayerName) -> String {
+        player_name.into()
+    }
+}
